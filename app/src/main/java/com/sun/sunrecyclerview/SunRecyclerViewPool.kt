241ccac989d4dbacafc87d8ff47e08ae9fe6407c
hengyangji <hengyangji@tencent.com>
2022-10-12 09:59:45 +0800

    定义跨列表缓存

---
+++ app/src/main/java/com/sun/sunrecyclerview/SunRecyclerViewPool.kt
@@ -0,0 +1,10 @@
+package com.sun.sunrecyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by hengyangji on 2022/10/12
+ */
+class SunRecyclerViewPool : RecyclerView.RecycledViewPool() {
+
+}
