3f3f15deae9286f1e097b33d8ca6113ffe6bf7c7
hengyangji <hengyangji@tencent.com>
2022-10-13 09:05:56 +0800

    定义自定义缓存

---
+++ app/src/main/java/com/sun/sunrecyclerview/SunRecyclerExtension.kt
@@ -0,0 +1,13 @@
+package com.sun.sunrecyclerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by hengyangji on 2022/10/12
+ */
+class SunRecyclerExtension : RecyclerView.ViewCacheExtension() {
+    override fun getViewForPositionAndType(recycler: RecyclerView.Recycler, position: Int, type: Int): View? {
+
+    }
+}
